c84ff1f111fa4ffaaae46393f686fe4469e9ca5f
alessaaandraaa <24100314@usc.edu.ph>
2026-01-15 15:56:18 +0800

    Vercel DB update

--- index.js
+++ index.js
@@ -22,3 +22,12 @@ mongoose
   .catch(() => {
     console.log("Failed to connect.");
   });
+
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
+
+export default app;
