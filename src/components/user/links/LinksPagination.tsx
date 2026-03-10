b4614900fd9e88da20f39eedda07115006157871
alessaaandraaa <24100314@usc.edu.ph>
2026-03-11 00:50:15 +0800

    feat: ADD for links

--- src/components/user/links/LinksPagination.tsx
+++ src/components/user/links/LinksPagination.tsx
@@ -1,8 +1,17 @@
+import AddLinksForm from "./AddLinksForm";
+import {
+  Dialog,
+  DialogContent,
+  DialogTrigger,
+  DialogTitle,
+} from "@/components/ui/dialog";
+
 type PaginationProps = {
   tasksPerPage: number;
   totalPosts: number;
   currentPage: number;
   paginate: (pageNumber: number) => void;
+  onAddLinks: (data: any) => void;
 };
 
 export default function LinksPagination({
@@ -10,6 +19,7 @@ export default function LinksPagination({
   totalPosts,
   currentPage,
   paginate,
+  onAddLinks,
 }: PaginationProps) {
   const totalPages = Math.ceil(totalPosts / tasksPerPage);
 
@@ -31,18 +41,26 @@ export default function LinksPagination({
         &lt;
       </button>
 
-      <button
-        style={{
-          fontSize: "8px",
-          backgroundColor: "rgba(255, 255, 255)",
-          padding: "1px",
-          height: "20px",
-          width: "20px",
-          borderRadius: "50%",
-        }}
-      >
-        +
-      </button>
+      <Dialog>
+        <DialogTrigger asChild>
+          <button
+            style={{
+              fontSize: "10px",
+              padding: "1px",
+              height: "20px",
+              width: "20px",
+              borderRadius: "50%",
+              fontWeight: "bold",
+            }}
+          >
+            +
+          </button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-106.25">
+          <DialogTitle>Add Task</DialogTitle>
+          <AddLinksForm onAddLinks={onAddLinks} />
+        </DialogContent>
+      </Dialog>
       <button
         style={{
           fontSize: "8px",
--- src/queries/links.queries.ts
+++ src/queries/links.queries.ts
@@ -1,6 +1,6 @@
 "use client";
 
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient, useMutation } from "@tanstack/react-query";
 import axios from "axios";
 
 const STALE_TIME = 1000 * 60 * 5;
@@ -15,5 +15,26 @@ export const getLinksQuery = () => {
       return response.data;
     },
     staleTime: STALE_TIME,
+  });
+};
+
+export const useAddLinksQuery = () => {
+  const queryClient = useQueryClient();
+  return useMutation<any, Error, any, { prevLinks?: any[] }>({
+    mutationFn: async (data) => {
+      const response = await axios.post(
+        `https://spotify-backend-eight-pink.vercel.app/api/links`,
+        data,
+      );
+      return response.data;
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["links"] });
+    },
+    onError: (_error, _variables, context: any) => {
+      if (context?.prevLinks) {
+        queryClient.setQueryData(["links"], context.prevLinks);
+      }
+    },
   });
 };
