44e487853f69dd903973f687815715a2a26944d0
alessaaandraaa <alissamay071006@gmail.com>
2026-07-31 23:51:29 +0800

    Feat: auth

--- src/components/homepage/Homepage.tsx
+++ src/components/homepage/Homepage.tsx
@@ -1,13 +1,8 @@
 import Hero from "./Hero";
-import CTA from "./CTA";
-import Features from "./Features";
-
 export default function Homepage() {
   return (
     <div>
       <Hero />
-      <Features />
-      <CTA />
     </div>
   );
 }
