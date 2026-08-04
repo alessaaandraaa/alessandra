597110b09a496ca59c09a90a27d7409a36f6c2e3
alessaaandraaa <alissamay071006@gmail.com>
2026-08-04 17:45:57 +0800

    feat: minor login button add lol

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -3,6 +3,7 @@ import ToDo from "@/components/user/todo/ToDo";
 import Playlist from "@/components/user/playlist/Playlist";
 import ImgLinksMain from "@/components/user/links/ImgLinksMain";
 import { authClient } from "@/lib/auth-client";
+import { Link } from "react-router-dom";
 
 export default function Dashboard() {
   const { isPending } = authClient.useSession();
@@ -12,7 +13,13 @@ export default function Dashboard() {
   }
 
   return (
-    <div className="flex gap-5 cursor-pointer group select-none justify-center w-full">
+    <div className="relative flex gap-5 cursor-pointer group select-none justify-center w-full">
+      <Link
+        to="/login"
+        className="absolute top-0 right-0 text-white text-xs bg-black/50 rounded-xl px-3 py-2"
+      >
+        Login
+      </Link>
       <ToDo />
       <div className="bg-black/25 p-5 rounded-2xl min-w-2xl max-w-2xl">
         <ImgLinksMain />
